cca24292df3520d243945642e85fe8632712ef1a
mellonnen <arvidgotthard98@gmail.com>
2022-03-07 17:01:09 +0100

    feat: added LRU get tests

--- lib/lru/lrutest.c
+++ lib/lru/lrutest.c
@@ -56,8 +56,40 @@ void test_put() {
   destroy_lru_cache(cache);
 }
 
+void test_get() {
+  lru_cache_t *cache = create_lru_cache(3);
+
+  put(cache, "limp", 1);
+  put(cache, "limpz", 2);
+  put(cache, "limpan", 2);
+
+  int *val;
+
+  val = get(cache, "limp");
+  printf("\ttest get head entry...");
+  assert(strcmp(cache->head->key, "limp") == 0);
+  assert(*val == 1);
+  printf("✅\n");
+
+  val = get(cache, "limpz");
+  printf("\ttest get entry in middle of LRU queue...");
+  assert(strcmp(cache->head->key, "limpz") == 0);
+  assert(strcmp(cache->head->lru_next->key, "limp") == 0);
+  assert(*val == 2);
+  printf("✅\n");
+
+  val = get(cache, "limpan");
+  printf("\ttest get tail entry...");
+  assert(strcmp(cache->head->key, "limpan") == 0);
+  assert(strcmp(cache->tail->key, "limp") == 0);
+  assert(*val == 2);
+  printf("✅\n");
+}
+
 int main(int argc, char *argv[]) {
   printf("Testing put:\n");
   test_put();
+  printf("Testing get:\n");
+  test_get();
   return 0;
 }
